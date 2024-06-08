4bde8506b4e4674b10ef479a9aecfe930c6edfef
Denis <polyashov.denis.24@gmail.com>
2024-06-08 13:18:52 +0300

    final2

--- src/main/java/com/example/cyber/config/SpringSecurityConfiguration.java
+++ src/main/java/com/example/cyber/config/SpringSecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.example.cyber.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
